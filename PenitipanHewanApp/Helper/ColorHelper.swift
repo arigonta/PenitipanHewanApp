627c7ebb8e6710b699e1b996ca6e5f89e97e6907
marco febriano <marco.ramadhani02@homecredit.co.id>
2020-07-29 09:52:31 +0700

    fix user tabbar

---
+++ PenitipanHewanApp/Helper/ColorHelper.swift
@@ -0,0 +1,16 @@
+//
+//  ColorHelper.swift
+//  PenitipanHewanApp
+//
+//  Created by Marco Ramadhani (ID) on 29/07/20.
+//  Copyright © 2020 JOJA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorHelper {
+    public static var instance = ColorHelper()
+    
+    let mainGreen: UIColor = .init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+}
---
+++ PenitipanHewanApp/User/Home/UserHomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UserHomeViewController.swift
+//  PenitipanHewanApp
+//
+//  Created by Marco Ramadhani (ID) on 29/07/20.
+//  Copyright © 2020 JOJA. All rights reserved.
+//
+
+import UIKit
+
+class UserHomeViewController: UIViewController {
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
