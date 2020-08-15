8f3e7d7110f613e0a21d39738b6b1c1517837b5f
Ari Gonta <arigonta@gmail.com>
2020-08-15 12:44:34 +0700

    add coolz and cool

--- PenitipanHewanApp/Petshop/Shop/Package/Detail Package/Controller/DetailPackageViewController.swift
+++ PenitipanHewanApp/Petshop/Shop/Package/Detail Package/Controller/DetailPackageViewController.swift
@@ -9,9 +9,10 @@
 
 import UIKit
 
-class DetailPackageViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+class DetailPackageViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var animalTypeTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var animalTypeLabel: UILabel!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var priceLabel: UILabel!
@@ -22,6 +23,7 @@ class DetailPackageViewController: UIViewController, UITextFieldDelegate, UIText
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var descriptionTextArea: UITextView!
     
+    var activeComponent: UIView?
     var pickerDeadline: PickerHelper?
     var pickerAnimal: PickerHelper?
     var deadlinePackage = ["7 Hari", "2 Minggu", "1 Bulan"]
@@ -35,7 +37,6 @@ class DetailPackageViewController: UIViewController, UITextFieldDelegate, UIText
         deadlineTextField.delegate = self
         animalTypeTextField.delegate = self
         nameTextField.delegate = self
-        descriptionTextArea.delegate = self
         
         descriptionTextArea.returnKeyType = UIReturnKeyType.done
         
@@ -98,13 +99,6 @@ class DetailPackageViewController: UIViewController, UITextFieldDelegate, UIText
         return true
     }
     
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if (text == "\n") {
-            textView.resignFirstResponder()
-        }
-        return true
-    }
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
@@ -127,6 +121,56 @@ extension DetailPackageViewController: PickerHelperDelegate {
             textField.text = value
         } else if textField == animalTypeTextField {
             textField.text = value
+        }
+    }
+}
+
+extension DetailPackageViewController {
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerForKeyboardNotifications()
+    }
+
+    override public func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        deregisterFromKeyboardNotifications()
+    }
+
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func deregisterFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWasShown(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size
+        let keyboardHeight = keyboardSize?.height ?? 0
+        if let scrollView = self.scrollView {
+            let contentInsets = UIEdgeInsets.init(top: 0.0, left: 0.0, bottom: keyboardHeight, right: 0.0)
+
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
+
+            var aRect: CGRect = self.view.frame
+            aRect.size.height -= keyboardHeight
+            if let activeComponent = self.activeComponent {
+                if !aRect.contains(activeComponent.frame.origin) {
+                    scrollView.scrollRectToVisible((activeComponent.frame), animated: true)
+                }
+            }
+        }
+    }
+
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        if let scrollView = self.scrollView {
+            let contentInsets = UIEdgeInsets.zero
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
         }
     }
 }
